96f9561bb5d5975a2aa1786ecd5c9574418c0901
David Kiv <funzeye@gmail.com>
2020-11-18 16:31:06 +0000

    try hash routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { createRouter, createWebHashHistory } from '@ionic/vue-router';
 import TabRoot from '../components/TabRoot.vue'
 
 
@@ -41,7 +41,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
